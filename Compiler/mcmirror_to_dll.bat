a000a2db5373d0b7b231c7b8202e0aa8c5d0cb37
Atrufulgium <atrufulgium@gmail.com>
2023-12-01 09:36:20 +0100

    Also generate MCMirror.xml documentation file
    * The batch script now also generates documentation.
    * Made the deletion a bit safer.

--- Compiler/mcmirror_to_dll.bat
+++ Compiler/mcmirror_to_dll.bat
@@ -1,4 +1,4 @@
-@rem Compiles ./MCMirror/* to ./MCMirror.dll
+@rem Compiles ./MCMirror/* to ./MCMirror.dll and ./MCMirror.xml.
 
 @rem The setup of the `MCMirror` directory is jank to such a degree that VS
 @rem doesn't let me compile it. As such, a script to do it manually.
@@ -8,13 +8,30 @@
 @rem TODO: At some point link the version to the relevant commit.
 @rem (Unfortunately needs to be a valid numeric x.y.z.w. Lame.)
 
-dotnet build .\MCMirror\MCMirror.csproj --no-dependencies --no-incremental --no-self-contained --property:OutputPath=.\build-temp --property:Version=0.0.0.1
-copy /Y .\MCMirror\build-temp\MCMirror.dll .\MCMirror.dll
-echo Y | rmdir /S .\MCMirror\build-temp
+dotnet build .\MCMirror\MCMirror.csproj --no-dependencies --no-incremental --no-self-contained --property:OutputPath=.\build-temp;GenerateDocumentationFile=true --property:Version=0.0.0.1 && (
+    del .\MCMirror.dll
+    del .\MCMirror.xml
+    copy /Y .\MCMirror\build-temp\MCMirror.dll .\MCMirror.dll
+    copy /Y .\MCMirror\build-temp\MCMirror.xml .\MCMirror.xml
+    del .\MCMirror\build-temp\MCMirror.dll
+    del .\MCMirror\build-temp\MCMirror.xml
+    del .\MCMirror\build-temp\MCMirror.deps.json
+    rmdir .\MCMirror\build-temp
+    echo(
+    echo(
+    echo Compilation succesful!
+) || (
+    echo(
+    echo(
+    echo Compilation failed!
+)
 
 @rem To use it in VS, right click the project, click "Add Project Reference",
 @rem click the "Browse" button in the bottom-right, and grab the MCMirror.dll.
 @rem Make sure that you do NOT include other references. This causes stuff to
 @rem fail.
+
+@rem To have the proper documentation, make sure you have the MCMirror.xml file
+@rem next to the dll file.
 
 @rem TODO: Add a sample .csproj that handles all reference stuff for you.
